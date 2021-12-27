f52cbc7cd3be08f55e8674f0a1545a1a2e3b863d
Pradeep Thombre <pradeepbthombre@gmail.com>
2021-12-28 00:44:53 +0530

    deployement

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,7 @@
 
 const mongoose = require("mongoose");
-mongoose.connect('mongodb://localhost:27017/csvDB',
+// mongoose.connect('mongodb://localhost:27017/csvDB',
+mongoose.connect('mongodb+srv://learningdemo068:HCsRDfy2vDs2KxN2@cluster0.zp24b.mongodb.net/csvUploader?retryWrites=true&w=majority',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true
