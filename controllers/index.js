23e1f519fc0548efd71e478b4a5cec16deefeae3
Pradeep Thombre <pradeepbthombre@gmail.com>
2021-12-29 09:58:29 +0530

    Update index.js

--- controllers/index.js
+++ controllers/index.js
@@ -14,18 +14,22 @@ module.exports.homePage = async (req, res) => {
 
 
 // upload file action 
-module.exports.uploadFile = (req, res) => {
-    File.uploadedFile(req, res, function (err) {
-        if (err) {
-            console.log("multer error ", err);
-        }
-    //    creating a file with as uploaded data
-        File.create({
-            name: req.file.originalname
-            , filePath: File.filePath + "/" + req.file.filename
+module.exports.uploadFile =async (req, res) => {
+    try{
+        let uploadeFile= await File.uploadedFile(req, res,  async function (err) {
+            if (err) {
+                console.log("multer error ", err);
+            }
+        //    creating a file with as uploaded data
+            let file= await File.create({
+                name: req.file.originalname
+                , filePath: File.filePath + "/" + req.file.filename
+            })
+            return res.redirect("back");
         })
-        return res.redirect("back");
-    })
+    }catch(err){
+        console.log('error',err);
+    }
 }
 module.exports.viewFile = async (req, res) => {
     let headers = [];
